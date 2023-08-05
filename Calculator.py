e12714e0e0a4a8443f9adce5cce33f407d4e2530
bhanupjha <mailtobhanup@gmail.com>
2023-08-06 00:03:01 +0530

    new program added

---
+++ Calculator.py
@@ -0,0 +1,9 @@
+# Create a calculator capable of performing addition, subtraction, multiplication and division operations on two numbers.
+a = 4
+b = 3
+
+print("Addition of",a,"and",b,"is: ", a+b)
+print("Subtraction of",a,"and",3,"is: ", a-b)
+print("Multiplication of",4,"and",3,"is: ", a*b)
+print("Addition of",a,"and",b,"is: ", a**b)
+print("Division of",a,"and",b,"is: ", a/b)
---
+++ Operator.py
@@ -0,0 +1,22 @@
+n = 15
+m = 7
+ans1 = n+m #Addition
+print("Addition of",n,"and",m,"is: ", ans1)
+
+ans2 = n-m #Subtration
+print("Subtraction of",n,"and",m,"is: ", ans2)
+
+ans3 = n*m #Multiplication
+print("Multiplication of",n,"and",m,"is: ", ans3)
+
+ans4 = n/m #Division
+print("Division of",n,"and",m,"is: ", ans4)
+
+ans5 = n%m #modulus
+print("Modulus of",n,"and",m,"is: ", ans5)
+
+ans6 = n//m #floor division
+print("Floor Division of",n,"and",m,"is: ", ans6)
+
+ans7  = 2**3 #Exponential
+print("Exponential of 2 and 3 is: ", ans7)
