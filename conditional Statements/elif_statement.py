3b437da7b59307fd34473ecde6a7a25b6e6a0606
bhanupjha <mailtobhanup@gmail.com>
2023-08-10 00:14:37 +0530

    new program added

---
+++ conditional Statements/elif_statement.py
@@ -0,0 +1,8 @@
+num = int(input("Enter the number: "))
+
+if (num < 0):
+    print("Number is negative.")
+elif (num == 0):
+    print("Number is Zero.")
+else:
+    print("Number is positive.")
---
+++ conditional Statements/if_statement.py
@@ -0,0 +1,7 @@
+number = int(input("Enter the number: "))
+print("Number is: ", number)
+
+# check if number is greater than 0
+if number > 0:
+    print('Number is positive.')
+
