f4e24b9da65159bf6521606626691e4f1171fa81
bhanupjha <mailtobhanup@gmail.com>
2023-08-11 21:17:51 +0530

    new program added

---
+++ Loops/ForLoop.py
@@ -0,0 +1,27 @@
+#*******itertaing over a string*******
+# name = 'Abhishek'
+# for i in name:
+#     print(i)
+
+#*******iterating over a list ********
+# colors = ["Red", "Green", "Blue", "Yellow"]
+# for x in colors:
+#     print(x)   
+#     for i in x:  # iteration over a string(x)
+#       print(i)
+      
+#********Range()**************
+#if we want to use for loop for a specific number of times
+# for k in range(5):
+#     print(k)  #By default loop starts from zero
+
+# for k in range(5):
+#     print(k + 1) 
+
+# for i in range(4,9):  # loop over a specific range
+#     print(i)
+
+# for g in range(1,9,2):   # range of parameter is 3
+#     print(g)   
+
+
