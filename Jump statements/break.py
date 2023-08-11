92c43c23407d942be0c2c0a7e79e54a615a11444
bhanupjha <mailtobhanup@gmail.com>
2023-08-12 00:36:40 +0530

    new program added

---
+++ Jump statements/break.py
@@ -0,0 +1,20 @@
+for i in range(5):
+    if(i==3):
+        break
+    print("The value of i is: ", i)
+print("Loop exit")    
+
+for x in range(12):
+    if(x==11):
+        break
+    print("5 *", x, "=", 5 * x)
+print("get out of the loop")
+
+for i in range(1,101,1):
+    print(i ,end=" ")
+    if(i==50):
+        break
+    else:
+        print("Mississippi")
+print("Thank you")
+
