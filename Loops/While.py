5c470f3f2b7c6e31174bb428847aefbf62f02d1c
bhanupjha <mailtobhanup@gmail.com>
2023-08-11 23:42:04 +0530

    new program added

---
+++ Loops/While.py
@@ -0,0 +1,20 @@
+count = 5
+while (count > 0):
+  print(count)
+  count = count - 1
+
+i = int(input("Enter the number: "))  
+print(i)
+while(i<20):
+  i = int(input("Enter the number: "))
+  print(i)
+
+print("Loop control Exit")  
+
+# ********Else with while loop************
+x = 5
+while (x > 0):
+    print(x)
+    x = x - 1
+else:
+    print('counter is 0')
