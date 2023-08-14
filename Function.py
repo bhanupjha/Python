650e8658c836d548ffca35dccf4ce0d9a612216b
bhanupjha <mailtobhanup@gmail.com>
2023-08-14 23:54:56 +0530

    new programm added

---
+++ Function.py
@@ -0,0 +1,46 @@
+a = 9
+b = 8
+gmean1 = (a*b)/(a+b)
+print(gmean1)
+
+c = 8
+d = 74
+gmean2 = (c*d)/(c+d)
+print(gmean2)
+
+# ********Built-in function**********
+# min(), mox(), len(), sum(), sum(), type(), range(), dict(), print(), etc.. 
+for k in range(5):
+    print(k) 
+
+a = "Hello"
+print(len(a))    
+print(type(a))
+
+
+# def calculateGmean(a, b):
+#   mean = (a*b)/(a+b)
+#   print(mean)
+
+# def isGreater(a, b):
+#   if(a>b):
+#     print("First number is greater")
+#   else:
+#     print("Second number is greater or equal")
+
+# def isLesser(a, b):
+#   pass
+  
+
+# a = 9
+# b = 8
+# # issGreater(a, b)
+# # calculateGmean(a, b)
+# gmean1 = (a*b)/(a+b)
+# print(gmean1)
+# c = 8
+# d = 74
+# # isGreater(c, d)
+# # calculateGmean(c, d)
+# gmean2 = (c*d)/(c+d)
+# print(gmean2)
