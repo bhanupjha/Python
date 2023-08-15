c9b8c1cddfba71a46938e8ca411f0914783d7a7b
bhanupjha <mailtobhanup@gmail.com>
2023-08-15 23:50:25 +0530

    new program added

--- Function.py
+++ Function.py
@@ -1,26 +1,27 @@
-a = 9
-b = 8
-gmean1 = (a*b)/(a+b)
-print(gmean1)
+# *********without using function***********
+# a = 9
+# b = 8
+# gmean1 = (a*b)/(a+b)
+# print(gmean1)
 
-c = 8
-d = 74
-gmean2 = (c*d)/(c+d)
-print(gmean2)
+# c = 8
+# d = 74
+# gmean2 = (c*d)/(c+d)
+# print(gmean2)
 
 # ********Built-in function**********
-# min(), mox(), len(), sum(), sum(), type(), range(), dict(), print(), etc.. 
-for k in range(5):
-    print(k) 
+# min(), mox(), len(), sum(), type(), range(), dict(), print(), etc.. 
+# for k in range(5):
+#     print(k) 
 
-a = "Hello"
-print(len(a))    
-print(type(a))
+# a = "Hello"
+# print(len(a))    
+# print(type(a))
 
-
+# *********user defined function**********
 # def calculateGmean(a, b):
 #   mean = (a*b)/(a+b)
-#   print(mean)
+#   print("The mean of", a, "and", b, "is:", mean)
 
 # def isGreater(a, b):
 #   if(a>b):
@@ -29,18 +30,18 @@ print(type(a))
 #     print("Second number is greater or equal")
 
 # def isLesser(a, b):
-#   pass
+#   pass         # we can define the function later
   
-
 # a = 9
 # b = 8
-# # issGreater(a, b)
-# # calculateGmean(a, b)
-# gmean1 = (a*b)/(a+b)
-# print(gmean1)
+# isGreater(a, b)
+# calculateGmean(a, b)
 # c = 8
 # d = 74
-# # isGreater(c, d)
-# # calculateGmean(c, d)
-# gmean2 = (c*d)/(c+d)
-# print(gmean2)
+# isGreater(c, d)
+# calculateGmean(c, d)
+
+# def name(fname, lname):
+#     print("Hello,", fname, lname)
+
+# name("Sam", "Wilson")
