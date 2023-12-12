66247df5c6589d98ac9c2d1038a2ffcd996255d9
bhanupjha <mailtobhanup@gmail.com>
2023-12-12 19:47:50 +0530

    new program added

--- GoodMorningSir.py
+++ GoodMorningSir.py
@@ -1,19 +1,16 @@
-import time
-timestamp = time.strftime('%H:%M:%S')
-print(timestamp)
-hour = int(time.strftime('%H'))
-print(hour)
-minute = int(time.strftime('%M'))
-print(minute)
-second = int(time.strftime('%S'))
-print(second)
 # https://docs.python.org/3/library/time.html#time.strftime
-
-if(hour > 5 and hour < 12):
-    print("Good Morning")
-elif(hour > 12 and hour < 17):
-    print("Good afternoon")
-elif(hour > 17 and hour < 20):
-    print("Good evening")
+import time
+timestamp = time.strftime('%H:%M:%S') # strftime is function which is gives hour and minute
+# The return value of strftime() is a string
+name = input("Enter you name: ")
+hour = int(time.strftime("%H"))
+# hour = int(input("Enter hour: "))
+# print(hour)
+if(hour >= 5 and hour < 12):
+    print("Good Morning", name)
+elif(hour >= 12 and hour < 17):
+    print("Good Afternoon", name)
+elif(hour >= 17 and hour <= 20):
+    print("Good Evening", name)
 else:
-    print("Good night")            
+    print("Good Night", name)            
