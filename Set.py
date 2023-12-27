3be9c7c2337c20dd205a4222c8a78e877f0e8fdd
bhanupjha <mailtobhanup@gmail.com>
2023-12-28 00:09:44 +0530

    new program added

---
+++ Set.py
@@ -0,0 +1,18 @@
+# s = {2, 4, 6, 2}
+# print(s)
+
+# info = {"carla", 19, 5.9, "carla"}
+# print(info)
+
+# ************usnig for loop**********
+# info = {"carla", 19, False, 5.9, "carla"}
+# for item in info:
+#     print(item)
+
+# Note:
+# x = {}
+# print(type(x))  # the type of empty set is dict(dictonary) 
+
+# if we want a type of empty set is set then we write like this:
+# x = set()
+# print(type(x))
