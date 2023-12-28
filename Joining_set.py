c71eebc702351e62b6cef1069798b3bd60d2e5e9
bhanupjha <mailtobhanup@gmail.com>
2023-12-28 21:42:10 +0530

    new program added

--- Joining_set.py
+++ Joining_set.py
@@ -6,7 +6,7 @@
 # print(cities3)
 
 # ****************update()******************
-# update() method add item into the existing set from another set
+# update() method print all items & add item into the existing set from another set
 # cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
 # cities2 = {"Tokyo", "Seoul", "Kabul", "Madrid"}
 # # before update
@@ -23,11 +23,38 @@
 # print(cities3)
 
 # ***************intersection_update()***********
-# intersection_update() method update into the existing set from another  set
+# intersection_update() method print similar item & update into the existing set from another  set
 # cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
 # cities2 = {"Tokyo", "Seoul", "Kabul", "Madrid"}
 # cities.intersection_update(cities2)
 # print(cities)
 
-# **************symmetric_diffrence**************
+# **************symmetric_difference()**************
+# symmetric_difference() method prints disimilar items to both the sets
+# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# cities2 = {"Tokyo", "Seoul", "Kabul", "Madrid"}
+# cities3 = cities.symmetric_difference(cities2)
+# print(cities3)
 
+# *************symmetric_difference_update()***********
+# symmetric_difference_update() method print disimilar items & update into the existing set
+# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# cities2 = {"Tokyo", "Seoul", "Kabul", "Madrid"}
+# cities.symmetric_difference_update(cities2)
+# print(cities)
+
+# **************difference()*******************
+# difference() method print only items that are only present in the original set and not in the both set 
+# difference() method return a new set
+# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# cities2 = {"Tokyo", "Seoul", "Kabul", "Madrid"}
+# cities3 = cities.difference(cities2)
+# print(cities3)
+
+# ***************difference_update()***************
+# difference_update() method print only items that are only present in the original set and not in the both set 
+# difference_update() method update into the existing set from another set
+# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# cities2 = {"Seoul", "Kabul", "Madrid"}
+# cities.difference_update(cities2)
+# print(cities)
