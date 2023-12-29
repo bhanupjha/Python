6cd218d1bade9cf207f8c6f6cf8dd5c7e44b8632
bhanupjha <mailtobhanup@gmail.com>
2023-12-29 23:35:56 +0530

    ne wprogram added

---
+++ Set_method.py
@@ -0,0 +1,9 @@
+# several in-built method used for manupulating of set
+
+# *********isdisjoint()***************
+# check if items of given set are present in another set
+# return false if item present else true
+# disjoint sets..> no element common
+# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# cities2 = {"Tokyo", "seoul", "kabul", "Madrid"}
+# print(cities.isdisjoint(cities2))
