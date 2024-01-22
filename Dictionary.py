7bc285a09004639b2d5ad0b9af3cc6064d090cee
bhanupjha <mailtobhanup@gmail.com>
2024-01-22 09:40:30 +0530

    new program added

---
+++ Dictionary.py
@@ -0,0 +1,6 @@
+# Dictionary are ordered collection of data items
+# Dictionary items are key-value pairs that are seperated by commas and enclosed with {} 
+info = {'name': 'karan', 'age': 19, 'eligible': True}
+print(info)
+
+# *********Accesing
