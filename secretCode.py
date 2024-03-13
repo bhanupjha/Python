513d303dbc5ed3f1b7d9cd2dc4c14ea8b542d34d
bhanupjha <mailtobhanup@gmail.com>
2024-03-13 11:00:52 +0530

    new program added

---
+++ secretCode.py
@@ -0,0 +1,9 @@
+mess = input("Enter you messages: ")
+secret = input("What you want code or decode: ")
+new_word = mess
+word = secret
+length = len()
+if secret == word:
+    print(length)
+    if length >= 3:
+        s = new_word.remove(0)
